66fbfeb3795b9faa23a1b11596305376cc1d3444
Shamsidddinshoma <qurbonovshamsiddin0423@gmail.com>
2023-02-04 14:15:44 +0500

    for masalalar

---
+++ main.js
@@ -0,0 +1,190 @@
+// 1
+// k va n butun son berilgan. K ni n marta chiqaruvchi dastur tuzing
+
+// function timeslog(k,n) {
+//     for(let i = 0; i < n; i++){
+//         console.log(k);
+//     }
+// }
+// timeslog(20,40)
+
+
+
+// 2
+// a, b sonlar beilgan. A b dan katta a va b orasidagi butun sonlarni chiqaruvchi dastur tuzing. A va B ni ozi ham chiqsin
+
+// function lognum(a,b) {
+//     for(let i = b; i < a + 1; i++ ){
+//         console.log(i);
+//     }
+// }
+
+
+
+
+// 3
+// a,b sonlar berilgan a b dan katta a va b orasidagi barcha butun sonlarni kamayish tartibida chiqaring a va b kirmasin
+
+// function lognum(a,b) {
+//     for(let i = a; i > b; i--){
+//         console.log(i);
+//     }
+// }
+
+// lognum(10,0)
+
+
+
+// 4
+// 1kg konfet 10000so'm berilgan. 1 kgdan 10 kggacha barchasin narxini chiqaruvchi dastur tuzing
+
+// let chocalate=[1,2,3,4,5,6,7,8,9,10];
+// function moneyChocolate(chocalate){
+//     for(let i= 0; i < chocalate.length; i++){
+//         let price =10000;
+//         console.log(price *= chocalate[i])
+//     }
+// }
+// moneyChocolate(chocalate)
+
+
+// 5
+// 1kg konfet 10000so'm. 0,1 kg dan 1 kg gacha bo'lgan konfet narxin toping
+// 1kg konfet 10000so'm. 1,2kg, 1,4, 1,6, 1,8, 2kg konfetlarni narxini toping
+
+// let kg = prompt('sonni kriting')
+// kg = Number(kg)
+
+// function olma(a) {
+//     let count = 10000
+//     return a * count
+// }
+// let resalt = olma(kg)
+// console.log(resalt);
+
+
+// 7
+// a,b sonlar berilgan. A dan b gacha barcha butun sonlarni yig'indisin topuvchi dastur yozing
+
+// function son(num){
+//     let a = 0
+//     for(let i = 0; i <= num; i++){
+//         a += i
+//     }
+//     return a
+// }
+
+// console.log(son(10));
+
+// 8
+// a,b sonlar berilgan. A dan b gacha barcha butun sonlarni ko'paytmasin topuvchi dastur yozing
+
+// function son(num){
+//     let a = 1
+//     for(let i = 1; i <= num; i++){
+//         a *= i
+//     }
+//     return a
+// }
+
+// console.log(son(10));
+
+
+
+// 9
+// a,b sonlar berilgan a dan b gacha bo'lgan sonlarni kvadratlarini yig'indisin topuvchi dastur toping
+
+// function son(num){
+//     let a = 1
+//     for(let i =0; i<= num;i++){
+
+//     }
+//     return a
+// }
+
+// console.log(son(10));
+
+
+// 11
+// n soni berilgan shu soni ichidan toqlarin ko'ruvchi dastur yozing
+
+// let n = 100
+
+// for(let i = 0; i <= n; i++){
+//     if(i % 2 == 0){
+//         console.log(i);
+//     }
+// }
+
+
+// 12
+// n soni berilgan shu sonlarni ichidan juftlarini koruvchi dastur yozing
+
+// let num = 100
+
+// for(let i = 0; i <= num;i++){
+//     if(i % 2 != 0){
+//         console.log(i);
+//     }
+// }
+
+
+
+// 13
+// n soni berilgan shu son ichidan faqat murakkab sonlarni aniqlovchi dastur yozing
+
+
+
+
+
+// 14
+// n soni berilgan shu sonlarni ichidan jufat va toq sonlarni kvadratlarini chiqaring
+
+
+
+
+
+
+// 15
+// n son berilgan n sonidan kamayish tartibida sonlar chiqarilsin
+
+// function sonlar(n){
+//     for(let i = n; i > 0; i--){
+//         console.log(i);
+//     }
+// }
+// sonlar(100)
+
+// 16
+// n son berilgan 0 va ngacha sonlar orasida 5 ga karralisini tashlab o'tib ketuvchi dastur tuzing
+
+// let n = 100;
+
+// for(let i = 0; i <= n; i++){
+//     if(i % 5 !== 0){
+//       console.log(i);
+//     }
+// }
+
+
+
+// 17
+// n butun son berilgan ngacha butun sonlarni ko'paytmasini topuvchi dastur tuzing
+
+// function sonlar(num) {
+//     let count = 1
+//     for(let i = 1; i <= num; i++){
+//         count *= i
+//     }
+//     return count
+// }
+
+// console.log(sonlar(10));
+
+
+
+
+
+
+
+
